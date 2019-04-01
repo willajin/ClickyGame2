913ab6ad879b530113bcf542574cb109ca8718dc
Willa Jin <jin.willa@gmail.com>
2019-03-31 17:49:16 -0700

    Updates

---
+++ precache-manifest.1bbe676d8ba914e53388b73c8828c3c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ClickyGame2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a372623a1f5b960c213f",
+    "url": "/ClickyGame2/static/js/runtime~main.1864acf8.js"
+  },
+  {
+    "revision": "dc26614834ddff1510de",
+    "url": "/ClickyGame2/static/js/main.c382ca02.chunk.js"
+  },
+  {
+    "revision": "d0919d735a230b7ca6c4",
+    "url": "/ClickyGame2/static/js/2.2a5dcd68.chunk.js"
+  },
+  {
+    "revision": "dc26614834ddff1510de",
+    "url": "/ClickyGame2/static/css/main.3eeecb15.chunk.css"
+  },
+  {
+    "revision": "d0919d735a230b7ca6c4",
+    "url": "/ClickyGame2/static/css/2.9226bf68.chunk.css"
+  },
+  {
+    "revision": "a358c184b58ca176bed4887fa5a5d210",
+    "url": "/ClickyGame2/index.html"
+  }
+];
